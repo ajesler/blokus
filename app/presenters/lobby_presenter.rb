92c919c34d5eba95f5b1d354cc8c617fce251f8c
aj esler <andrew.esler@powershop.co.nz>
2015-07-28 10:00:51 +1200

    Removing presenter wrapper for games in the lobby

--- app/presenters/lobby_presenter.rb
+++ app/presenters/lobby_presenter.rb
@@ -4,20 +4,6 @@ class LobbyPresenter
 	end
 
 	def games
-		@games ||= construct_lobby_games
-	end
-
-	LobbyGame = Struct.new(:id, :player_one, :player_two, :player_three, :player_four)
-
-	private
-
-	def construct_lobby_games
-		games = []
-
-		@user.games.order(id: :desc).each do |game|
-			games << LobbyGame.new(game.id, game.players[0].name, game.players[1].name, game.players[2].name, game.players[3].name)
-		end
-
-		games
+		@games ||= @user.games.order(id: :desc)
 	end
 end
