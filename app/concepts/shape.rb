631a91d08f1870471129ff96736528894a59652a
aj esler <andrew.esler@powershop.co.nz>
2015-07-28 15:33:35 +1200

    Added shape isomer generation

--- app/concepts/shape.rb
+++ app/concepts/shape.rb
@@ -1,4 +1,6 @@
 require 'matrix'
+require 'set'
+require_relative 'transforms'
 
 class Shape
 	attr_reader :name, :definition
@@ -11,4 +13,23 @@ class Shape
 	def size
 		@definition.column_count
 	end
+
+  def isomers
+    @isomers ||= calculate_isomers
+  end
+
+  private
+
+  def calculate_isomers
+    Transforms.transforms.each.with_object(Set.new) do |transform, isomers|
+      isomer = transform.definition * self.definition
+
+      isomer_at_origin = MatrixConversions.move_to_origin(isomer)
+      point_set = MatrixConversions.to_point_set(isomer_at_origin)
+
+      if !isomers.include?(point_set)
+        isomers.add(point_set)
+      end
+    end
+  end
 end
--- spec/concepts/shape_spec.rb
+++ spec/concepts/shape_spec.rb
@@ -1,15 +1,31 @@
 require 'rails_helper'
 
 RSpec.describe Shape do
-	let(:name) { :test_shape }
-	let(:definition) { Matrix[[1, 1, 2, 0, 1],[0, 1, 1, 2, 2]] }
-	let(:shape) { Shape.new(name, definition) }
 
-	describe "#size" do
-		subject { shape.size }
+  describe "#size" do
+    let(:name) { :test_shape }
+    let(:definition) { Matrix[[1, 1, 2, 0, 1],[0, 1, 1, 2, 2]] }
+    let(:shape) { Shape.new(name, definition) }
+  
+    subject { shape.size }
 
-		it "should return the number of squares in the shape" do
-			is_expected.to eq 5
-		end
-	end
+    it "should return the number of squares in the shape" do
+      is_expected.to eq 5
+    end
+  end
+
+  describe "#isomers" do
+    let(:x) { Shape.new("X", Matrix[[0, 1, 1, 2, 1], [1, 0, 1, 1, 2]]) }
+    let(:one) { Shape.new("1", Matrix[[0],[0]]) }
+    let(:i3) { Shape.new("I3", Matrix[[0, 0, 0],[0, 1, 2]]) }
+    let(:f) { Shape.new("F", Matrix[[1, 1, 2, 0, 1],[0, 1, 1, 2, 2]]) }
+    let(:y) { Shape.new("Y", Matrix[[0, 0, 0, 1, 0],[0, 1, 2, 2, 3]]) }
+    let(:shapes) { [[x, 1], [one, 1], [i3, 2], [f, 8], [y, 8]] }
+
+    it "correctly calculates all isomers for various shapes" do
+      shapes.each do |shape, expected_isomer_count|
+        expect(shape.isomers.size).to eq expected_isomer_count
+      end
+    end
+  end
 end
