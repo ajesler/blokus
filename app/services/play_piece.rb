8eadfedacc451d5491f0d01cd36a5caed2244f8a
aj esler <andrew.esler@powershop.co.nz>
2015-07-31 10:33:06 +1200

    Added lock on game to PlayPiece

--- app/services/play_piece.rb
+++ app/services/play_piece.rb
@@ -6,38 +6,40 @@ class PlayPiece
 	end
 
 	def call
-		if @game.finished?
-			return false
+		@game.with_lock do 
+			if @game.finished?
+				return false
+			end
+
+			if @game.active_player != @player
+				return false
+			end
+
+			if @coordinates.empty?
+				@player.turns.create!(shape: nil, transform: nil, x: nil, y: nil)
+				return true
+			end
+
+			validate_move = ValidateMove.new(@player, @coordinates)
+			move_is_valid = validate_move.call
+
+			if !move_is_valid
+				return false
+			end
+
+			identified_shape = validate_move.identified_shape
+			if !identified_shape.identified?
+				return false
+			end
+
+			colour = @game.colours[@game.turns.length % @game.colours.length]
+			shape = identified_shape.shape
+			transform = identified_shape.transform
+			position = identified_shape.position
+
+			@player.turns.create!(shape: shape, transform: transform, x: position.x, y: position.y)
+
+			move_is_valid
 		end
-
-		if @game.active_player != @player
-			return false
-		end
-
-		if @coordinates.empty?
-			@player.turns.create!(shape: nil, transform: nil, x: nil, y: nil)
-			return true
-		end
-
-		validate_move = ValidateMove.new(@player, @coordinates)
-		move_is_valid = validate_move.call
-
-		if !move_is_valid
-			return false
-		end
-
-		identified_shape = validate_move.identified_shape
-		if !identified_shape.identified?
-			return false
-		end
-
-		colour = @game.colours[@game.turns.length % @game.colours.length]
-		shape = identified_shape.shape
-		transform = identified_shape.transform
-		position = identified_shape.position
-
-		@player.turns.create!(shape: shape, transform: transform, x: position.x, y: position.y)
-
-		move_is_valid
 	end
 end
