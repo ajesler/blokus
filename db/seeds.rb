b0f67239a40b5c33ee851abad45a3bc20552d1d7
aj esler <andrew.esler@powershop.co.nz>
2015-07-30 15:17:22 +1200

    Update seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,22 +8,23 @@ User.create!([
 
 game = Game.new
 game.players.build(user: User.find(1))
-game.players.build(user: User.find(2))
-game.players.build(user: User.find(3))
-game.players.build(user: User.find(4))
 game.save!
+
+game.players.create!(user: User.find(2))
+game.players.create!(user: User.find(3))
+game.players.create!(user: User.find(4))
 
 Turn.create!([
   {player_id: 1, shape: "T4", transform: "rotate_90_anticlockwise", x: 0, y: 0},
-  {player_id: 4, shape: "Y", transform: "rotate_90_anticlockwise", x: 16, y: 0},
   {player_id: 2, shape: "Z5", transform: "reflect_in_y_x", x: 17, y: 17},
   {player_id: 3, shape: "F", transform: "identity", x: 0, y: 17},
+  {player_id: 4, shape: "Y", transform: "rotate_90_anticlockwise", x: 16, y: 0},
   {player_id: 1, shape: "F", transform: "rotate_180", x: 1, y: 2},
-  {player_id: 4, shape: "L5", transform: "reflect_in_y_neg_x", x: 12, y: 1},
   {player_id: 2, shape: "I5", transform: "rotate_90_clockwise", x: 12, y: 16},
   {player_id: 3, shape: "I5", transform: "rotate_90_clockwise", x: 3, y: 17},
+  {player_id: 4, shape: "L5", transform: "reflect_in_y_neg_x", x: 12, y: 1},
   {player_id: 1, shape: "U", transform: "rotate_90_clockwise", x: 4, y: 3},
-  {player_id: 4, shape: "Z5", transform: "identity", x: 10, y: 3},
   {player_id: 2, shape: "Y", transform: "reflect_in_y_x", x: 8, y: 15},
-  {player_id: 3, shape: "W", transform: "identity", x: 8, y: 16}
+  {player_id: 3, shape: "W", transform: "identity", x: 8, y: 16},
+  {player_id: 4, shape: "Z5", transform: "identity", x: 10, y: 3}
 ])
