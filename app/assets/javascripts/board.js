936077e9365969b97e23a57117393dfc8d66c655
aj esler <andrew.esler@powershop.co.nz>
2015-07-31 16:04:00 +1200

    Rewrite of Board so that all functions are on the Board prototype.
    Results in cleaner code now that it is possible to map and filter all the things!

--- app/assets/javascripts/board.js
+++ app/assets/javascripts/board.js
@@ -1,9 +1,8 @@
 var Board = (function(){
-  var BLANK = "";
+  var EMPTY = "";
 
   var Board = function(size){
     if(size instanceof Array){
-      // creating a new board with a given grid
       var boardArray = size;
       this.size = boardArray.length;
       this.board = boardArray;
@@ -18,7 +17,7 @@ var Board = (function(){
       for(var y = 0; y < this.size; y++){
         this.board[y] = new Array(this.size);
         for(var x = 0; x < this.size; x++){
-          this.board[y][x] = BLANK;
+          this.board[y][x] = EMPTY;
         }
       }
     }
@@ -33,154 +32,107 @@ var Board = (function(){
     }
   };
 
-  var pointOnBoard = function(x, y){
-    if(typeof(y) === "undefined" && x instanceof Array && x.length == 2){
-      y = x[1];
-      x = x[0];
-    }
-
-    if (this.size === "undefined") {
-      throw "undefined board size in board.pointOnBoard()";
-    }
-
-    if(x > this.size - 1) { 
-      return false
-    }
-    if(y > this.size - 1){
-      return false;
-    }
-    if(x < 0){
-      return false;
-    }
-    if(y < 0){
-      return false;
-    }
-    return true;
-  }
-
-  var allCoordinatesOnBoard = function(coordinates){
-    var self = this;
-    var allOnBoard = coordinates.columns().reduce(function(validity, currentValue){
-      var isValid = pointOnBoard.call(self, currentValue[0], currentValue[1]);
-      return validity && isValid;
-    }, true);
-
-    return allOnBoard;
-  };
-
-  var offsetSquares = function(x, y, offsets){
-    var squares = [];
-
-    offsets.forEach(function(point){
-      var nx = x + point[0];
-      var ny = y + point[1];
-      squares.push([nx, ny]);
-    });
-
-    var self = this;
-    var validSquares = squares.filter(function(square){
-      return pointOnBoard.call(self, square);
-    });
-    return validSquares;
-  };
-
-  var adjacentSquares = function(x, y){
-    return offsetSquares.call(this, x, y, [[0, 1], [1, 0], [0, -1], [-1, 0]]);
-  }
-
-  var adjacentCorners = function(x, y){
-    return offsetSquares.call(this, x, y, [[1, 1], [-1, 1], [1, -1], [-1, -1]]);
-  }
-
-  var touchesACornerOfSameColour = function(coordinates, colour){
-    var cornerCoords = [];
-    var self = this;
-    coordinates.columns().forEach(function(column, index){
-      var corners = adjacentCorners.call(self, column[0], column[1]);
-
-      corners.forEach(function(point){
-        if(!Utils.containsElement(cornerCoords, point)){
-          cornerCoords.push(point);
-        }
+  Board.prototype.allCoordinatesValid = function(coordinates) {
+    return coordinates.columns()
+      .map(function(point){
+        return this.isOnBoard(point[0], point[1])
+      }, this)
+      .every(function(isValid){
+        return isValid;
       });
-    });
-
-    coordinates.columns().forEach(function(point){
-      if(Utils.containsElement(cornerCoords, point)){
-        Utils.removeElement(point);
-      }
-    });
-
-    var board = this;
-    return cornerCoords.map(function(point){
-      return board.square(point[0], point[1]) === colour;
-    }).some(function(result){
-      return result == true
-    });
   };
 
-  var doesNotTouchEdgesOfTheSameColour = function(coordinates, colour){
-    var adjacentCoords = [];
+  Board.prototype.adjacentTo = function(x, y) {
+    return this.offsetSquares(x, y, [[0, 1], [1, 0], [0, -1], [-1, 0]]);
+  };
 
-    var self = this;
-    coordinates.columns().forEach(function(column){
-      var adjacent = adjacentSquares.call(self, column[0], column[1]);
+  Board.prototype.adjacentCorners = function(x, y) {
+    return this.offsetSquares(x, y, [[1, 1], [-1, 1], [1, -1], [-1, -1]]);
+  };
 
-      adjacent.forEach(function(point){
-        if(!Utils.containsElement(adjacentCoords, point)){
-          adjacentCoords.push(point);
-        }
+  Board.prototype.offsetSquares = function(x, y, offsetPoints) {
+    return offsetPoints
+      .map(function(point){
+        return [point[0]+x, point[1]+y];
+      }, this)
+      .filter(function(point){
+        return this.isOnBoard(point[0], point[1]);
+      }, this);
+  };
+
+  Board.prototype.hasAdjacentCornerOfColour = function(coordinates, colour) {
+    return coordinates.columns()
+      .map(function(column){
+        return this.adjacentCorners(column[0], column[1])
+          .map(function(point){
+            return this.square(point[0], point[1]) === colour;
+          }, this)
+          .some(function(hasAdjacentCornerOfColour){
+            return hasAdjacentCornerOfColour;
+          });
+      }, this)
+      .some(function(isAdjacent){
+        return isAdjacent;
       });
-    });
+  };
 
-    coordinates.columns().forEach(function(point){
-      if(Utils.containsElement(adjacentCoords, point)){
-        Utils.removeElement(point);
-      }
-    });
+  Board.prototype.doesNotTouchSameColourEdge = function(coordinates, colour) {
+    return coordinates.columns()
+      .map(function(column){
+        return this.adjacentTo(column[0], column[1])
+          .map(function(point){
+            return this.square(point[0], point[1]) === colour;
+          }, this)
+          .every(function(hasAdjacent){
+            return !hasAdjacent;
+          });
+      }, this)
+      .every(function(isNotAdjacent){
+        return isNotAdjacent;
+      });
+  };
 
-    var board = this;
-    var touchesEdge = adjacentCoords.map(function(point){
-      return board.square(point[0], point[1]) === colour;
-    }).some(function(result){
-      return result === true
-    });
-
-    return !touchesEdge;
-  }
-
-  var inBoardCorner = function(coordinates) {
+  Board.prototype.isABoardCorner = function(coordinates) {
     var max_index = this.size - 1
     var corners = [
       [0, 0],
       [0, max_index],
       [max_index, 0],
       [max_index, max_index]
-    ]
+    ];
 
-    var isInBoardCorner = false;
-    coordinates.columns().forEach(function(column){
-      var coversCorner = corners
-                          .map(function(cornerPoint){
-                            var isCorner = cornerPoint[0] === column[0] && cornerPoint[1] === column[1];
-                            return isCorner;                
-                          }).some(function(element){
-                            return element;
-                          });
-      if (coversCorner) {
-        isInBoardCorner = true;
-      }
-    });
-
-    return isInBoardCorner;
+    return coordinates.columns()
+      .map(function(column){
+        return corners.some(function(point){
+          return column[0] === point[0] && column[1] === point[1];
+        })
+      }, this)
+      .some(function(coversACorner){
+        return coversACorner;
+      });
   };
 
   Board.prototype.isOnBoard = function(x, y) {
-    return pointOnBoard.call(this, x, y);
+    if(typeof(y) === "undefined" && x instanceof Array && x.length == 2){
+      y = x[1];
+      x = x[0];
+    }
+
+    if (this.size === "undefined") {
+      throw "undefined board size in board.isOnBoard()";
+    }
+
+    if(x > this.size - 1 || y > this.size - 1){
+      return false;
+    }
+    if(x < 0 || y < 0){
+      return false;
+    }
+    return true;
   };
 
   Board.prototype.isValidMove = function(coordinates, playerColour, isFirstTurn) {
-    var onBoard = allCoordinatesOnBoard.call(this, coordinates);
+    var onBoard = this.allCoordinatesValid(coordinates);
     if (!onBoard) {
       return false;
     }
@@ -192,11 +144,12 @@ var Board = (function(){
 
     var isValid = false;
     if (isFirstTurn) {
-      var coversABoardCorner = inBoardCorner.call(this, coordinates);
+      // var coversABoardCorner = inBoardCorner.call(this, coordinates);
+      var coversABoardCorner = this.isABoardCorner(coordinates);
       isValid = onBoard && areEmpty && coversABoardCorner;
     } else {
-      var touchesCornerOfSameColour = touchesACornerOfSameColour.call(this, coordinates, playerColour);
-      var noSharedEdges = doesNotTouchEdgesOfTheSameColour.call(this, coordinates, playerColour);
+      var touchesCornerOfSameColour = this.hasAdjacentCornerOfColour(coordinates, playerColour);
+      var noSharedEdges = this.doesNotTouchSameColourEdge(coordinates, playerColour);
 
       isValid = onBoard && areEmpty && touchesCornerOfSameColour && noSharedEdges;
     }
@@ -221,23 +174,76 @@ var Board = (function(){
     });
   };
 
+  Board.prototype.allSquaresOfColour = function(colour) {
+    var coords = [];
+
+    this.forEachSquare(function(x, y, contents){
+      if(contents === colour) {
+        coords.push([x, y]);
+      }
+    });
+
+    return coords;
+  };
+
+  Board.prototype.isAdjacentToColour = function(x, y, colour) {
+    var adjacent = this.adjacentTo(x, y)
+      .map(function(point){
+        var isAdjacent = this.square(point[0], point[1]) === colour;
+        return isAdjacent;
+      }, this)
+      .some(function(isAdjacent){
+        return isAdjacent;
+      }, this);
+
+    return adjacent;
+  };
+
+  Board.prototype.isEmpty = function(x, y) {
+    return this.square(x, y) === EMPTY;
+  };
+
+  Board.prototype.forEachFreeCorner = function(colour, callback) {
+    var isEmptySquare = function isEmptySquare(point) {
+      return this.isEmpty(point[0], point[1]);
+    }.bind(this);
+
+    var isNotAdjacent = function isNotAdjacent(point) {
+      return !this.isAdjacentToColour(point[0], point[1], colour);
+    }.bind(this);
+
+    var possibleSquares = [];
+    this.allSquaresOfColour(colour).forEach(function(colourPoint) {
+      this.adjacentCorners(colourPoint[0], colourPoint[1])
+          .filter(isEmptySquare)
+          .filter(isNotAdjacent)
+          .forEach(function(point){
+            if(!possibleSquares.includes(point)){
+              possibleSquares.push(point);
+            }
+          }, this);
+    }, this);
+
+    possibleSquares
+      .forEach(function(point){
+        callback(point[0], point[1]);
+      }, this);
+  };
+
   Board.prototype.coordinatesAreAllEmpty = function(coordinates) {
     if(coordinates instanceof Array){
       coordinates = new Matrix(coordinates);
     }
 
-    var free = [];
+    var isEmptySquare = function isEmptySquare(point) {
+      return this.isEmpty(point[0], point[1]);
+    }.bind(this);
 
-    for(var i = coordinates.columnCount() - 1; i >= 0; i--){
-      var point = coordinates.column(i);
-      var x = point[0];
-      var y = point[1];
-
-      var isFree = this.square(x, y) === BLANK;
-      free.push(isFree);
-    }
-
-    return free.every(function(square){ return square == true });
+    return coordinates.columns()
+      .map(isEmptySquare)
+      .every(function(isEmpty){
+        return isEmpty;
+      });
   };
 
   return Board;
